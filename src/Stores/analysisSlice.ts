a680d112e7fd4f01724cdf79559aa392f4481b0e
matsuyoshi <matsu1637@gmail.com>
2022-10-16 21:14:43 +0900

    fix: Stateの中身変更による修正

--- src/Stores/analysisSlice.ts
+++ src/Stores/analysisSlice.ts
@@ -1,88 +1,92 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { RootState } from '../app/store'
 import axios from 'axios'
-
+import { CheckJsDirective } from 'typescript'
 
 // API URL PASS
 const endPoint = 'https://opendata.resas-portal.go.jp'
 
-// stateの型
-export type prefectures = [
-  {
-    id:string,
-    name:string
-  }
-]
-
-
-
-
+// state全体
 export type initialState = {
-  prefectures?:prefectures
+  prefectures: [
+    {
+      id: string
+      name: string
+      checked: boolean
+    }?,
+  ]
 }
 
 // 初期値
 const initialState: initialState = {
-  prefectures:[
-    {
-      id:"",
-      name:""
-    }
-  ],
-
+  prefectures: [],
 }
 
-// 都道府県一覧
-export const fetchAsyncPrefectures = createAsyncThunk<prefectures, undefined, {rejectValue: string;}>(
-  "prefectures/get",
-  async (auth,thankAPI) => {
-    try {
-      alert("API取得開始")
-      console.log(endPoint + '/api/v1/prefectures')
-      const res = await axios.get(endPoint + '/api/v1/prefectures', {
-        headers: {
-          'X-Api-Key': '9ai5GVToFKxkq6oShMKRcaIhUJ9VuQG1KB35xd7p',
-        },
+/// APIレスポンス
 
-        method: 'GET',
-      })
-      alert("API取得完了")
-      return res.data["result"]
-
-    } catch (e) {
-      alert('error発生')
-      if (axios.isAxiosError(e) && e.response) {
-        return thankAPI.rejectWithValue(e.response.data)
-      }
-    
-    }
+// 都道府県情報レスポンス
+export type resPrefectures = [
+  {
+    prefCode: number
+    prefName: string
   },
-)
+]
+
+// 都道府県一覧
+export const fetchAsyncPrefectures = createAsyncThunk<
+  resPrefectures,
+  undefined,
+  { rejectValue: string }
+>('prefectures/get', async (auth, thankAPI) => {
+  try {
+    console.log(endPoint + '/api/v1/prefectures')
+    const res = await axios.get(endPoint + '/api/v1/prefectures', {
+      headers: {
+        'X-Api-Key': '9ai5GVToFKxkq6oShMKRcaIhUJ9VuQG1KB35xd7p',
+      },
+      method: 'GET',
+    })
+    return res.data['result']
+  } catch (e) {
+    if (axios.isAxiosError(e) && e.response) {
+      return thankAPI.rejectWithValue(e.response.data)
+    }
+  }
+})
+
 
 export const analysisSlice = createSlice({
-  name: "analysis",
+  name: 'analysis',
   initialState: initialState,
   reducers: {
+    setCheckBok(state, action) {
+      state.prefectures.filter(n => n?.id === action.payload)[0]?.checked
+    },
   },
   extraReducers: (builder) => {
-
     builder.addCase(fetchAsyncPrefectures.fulfilled, (state, action) => {
-      console.log(action.payload)
-      alert("成功")
-      if(Array.isArray(action.payload)){
-        state.prefectures = action.payload
+      if (Array.isArray(action.payload)) {
+        state.prefectures = []
+        action.payload.map((element) => {
+          state.prefectures?.push({
+            id: element.prefCode.toString(),
+            name: element.prefName,
+            checked: false,
+          })
+        })
       }
     })
-    
-    builder.addCase(fetchAsyncPrefectures.rejected, (state, action) => {alert("失敗")})
-      
-    builder.addCase(fetchAsyncPrefectures.pending, (state, action) => {
-      
+
+    builder.addCase(fetchAsyncPrefectures.rejected, (state, action) => {
     })
+
+    builder.addCase(fetchAsyncPrefectures.pending, (state, action) => {})
+
+    
   },
 })
 
-export const selectLoginState = (state: RootState) => state.test
+export const selectAnalysisState = (state: RootState) => state.analysis
 export const {} = analysisSlice.actions
 
 export default analysisSlice.reducer
